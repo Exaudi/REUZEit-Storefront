ea0b13d55c62545c4bd124cd59adf860d6fe5212
Aditya Sharma <kua.sharma@gmail.com>
2022-05-22 20:45:41 -0700

    new page

---
+++ src/components/product selection/ProductSelection.jsx
@@ -0,0 +1,14 @@
+//import React, {Component} from 'react';
+import Nav from './navBar/navbar'
+//import Dropdown from './dropdown/dropDownMenu';
+
+export default function shop(){
+    return(
+        
+        <body>
+            <Nav />
+            <h2>CART</h2>
+         
+        </body>
+    );
+}
